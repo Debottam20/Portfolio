4905bd0efeaba7ab2abe79f8b7dcd951605a1987
Debottam20 <d.mukherjee2003.official@gmail.com>
2024-09-11 12:31:47 +0530

    Final

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+  var dataText = ["Singer.", "Gamer.", "Web Developer.", "Java Lover.", "Python Enthusiast.", "Trying To Learn New Things!!"];
+
+  function typeWriter(text, i, fnCallback) {
+    if (i < (text.length)) {
+      document.getElementById("typing").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+      setTimeout(function () {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+
+    else if (typeof fnCallback == 'function') {
+      setTimeout(fnCallback, 1500);
+    }
+  }
+
+  function StartTextAnimation(i) {
+    if (typeof dataText[i] == 'undefined') {
+      setTimeout(function () {
+        StartTextAnimation(0);
+      });
+    }
+
+    if (i < dataText[i].length) {
+      typeWriter(dataText[i], 0, function () {
+        StartTextAnimation(i + 1);
+      });
+    }
+  }
+  StartTextAnimation(0);
+});
